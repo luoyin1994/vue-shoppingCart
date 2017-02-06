35cd469b515db4dbcad246fcd2fa6639a512e316
luoyin <luoyin1994@icloud.com>
2017-02-06 19:14:22 +0800

    handle item check with select function

--- public/js/vuejs/pages/address.js
+++ public/js/vuejs/pages/address.js
@@ -1,36 +1,62 @@
 new Vue({
     el      : '#app',
     data    : {
-        addressList: [],
+        limitNumber       : 3,
+        addressList       : [],
+        deliveryMethodList: [],
+        arrowTopFlag      : false,
+    },
+    filters : {
+        formatMoney: function (value) {
+            return `￥ ${value.toFixed()}元`
+        }
     },
     mounted : function () {
         this.$nextTick(function () {
-            this.getAddressList()
+            this.getListAddressList()
+            this.getDeliveryMethodList()
         })
 
     },
     computed: {
         filterAddress: function () {
-            return this.addressList.slice(0, 3)
+            return this.addressList.slice(0, this.limitNumber)
         }
     },
     methods : {
-        getAddressList: function () {
+        getListAddressList   : function () {
             this.$http.get('/data/addressData.json').then(res => {
                 if (res.data.status == 0) {
                     this.addressList = res.data.result
                 }
             })
         },
-        selectAddress : function (item) {
-            if (typeof item.selected == 'undefined') {
+        getDeliveryMethodList: function () {
+            this.$http.get('/data/deliveryMethodData.json').then(res => {
+                if (res.data.status == 0) {
+                    this.deliveryMethodList = res.data.result
+                }
+            })
+        },
+        selectDeliveryMethod : function (item) {
+            this.deliveryMethodList.map(item => {
+                item.selectDefault = false
                 this.$set(item, 'selected', false)
-            }
-            this.addressList.forEach(function (value, i) {
-                console.log(value.selected)
-                value.selected = false
             })
             item.selected = true
-        }
+        },
+        selectAddress        : function (item) {
+            this.addressList.map(item => {
+                item.selectDefault = false
+                this.$set(item, 'selected', false)
+            })
+            item.selected = true
+        },
+        loadMore             : function () {
+            this.limitNumber == 3 ? this.limitNumber = this.addressList.length : this.limitNumber = 3
+            this.arrowTopFlag = !this.arrowTopFlag
+        },
+
     }
+
 })
